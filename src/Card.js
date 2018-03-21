c81e7fc41e475ebff579562be41d89c54228cf02
IoPhoenix <olga.fomin26@gmail.com>
2018-03-20 23:45:56 -0700

    First commit

---
+++ src/Card.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Card extends Component {
+
+    changeCard = (e) => {
+        console.log(this);
+    }
+
+    render() {
+        return (
+            <div onClick={this.changeCard} className='tc bg-light-green b--green dib br3 pa3 ma2 bw2 grow shadow-5'>
+                <img src={`https://robohash.org/${this.props.id}?200x200`} width='200' height='200' alt={`robot named ${this.props.name}`} />
+                <div>
+                    <h2>{this.props.name}</h2>
+                    <p>{this.props.email}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,102 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Leanne Graham',
+    username: 'Bret',
+    email: 'Sincere@april.biz',
+    hobbies: [
+      'Computer programming', 'Scrapbooking'
+    ],
+    movie: 'Star Wars trilogy'
+  },
+  {
+    id: 2,
+    name: 'Ervin Howell',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv',
+    hobbies: [
+      'Taxidermy', 'Yoga', 'Bird Watching'
+    ],
+    movie: 'Transformers'
+  },
+  {
+    id: 3,
+    name: 'Clementine Bauch',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net',
+    hobbies: [
+      'Photography', 'Wood carving'
+    ],
+    movie: 'The Matrix'
+  },
+  {
+    id: 4,
+    name: 'Patricia Lebsack',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org',
+    hobbies: [
+      'Brazilian jiu-jitsu', 'Jazz Music', ''
+    ],
+    movie: 'Ex Machina'
+  },
+  {
+    id: 5,
+    name: 'Chelsey Dietrich',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca',
+    hobbies: [
+      'Knitting', 'Kombucha brewing', '3D printing'
+    ],
+    movie: 'The Terminator'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info',
+    hobbies: [
+      'Macrame'
+    ],
+    movie: 'The Animatrix'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz',
+    hobbies: [
+      'Pottery', 'Fishing'
+    ],
+    movie: 'Robots'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me',
+    hobbies: [
+      'Vehicle restoration', 'Comic book collecting'
+    ],
+    movie: 'RoboCop'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io',
+    hobbies: [
+      'Whale watching', 'Traveling'
+    ],
+    movie: 'Blade Runner'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz',
+    hobbies: [
+      'Satellite watching', 'Candle making', 'Acting'
+    ],
+    movie: 'Metropolis'
+  }
+];
