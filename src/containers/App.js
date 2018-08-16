09f49c839eee7523072db80756f2aa91ce807c20
IoPhoenix <olga.fomin26@gmail.com>
2018-08-16 15:56:19 -0700

    Added requestRobots action

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,52 +6,41 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import ErrorBoundary from '../components/ErrorBoundary';
 
-import {setSearchField} from '../actions';
+import {setSearchField, requestRobots} from '../actions';
 
 const mapStateToProps = (state) => {
     return {
-        searchField: state.searchField
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
     }
 }
 
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
     }
 }
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: []
-        }
-    }
 
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({robots: users}))
-        .catch(err => console.log('Could not fetch robots: ', err));
+       this.props.onRequestRobots();
     }
 
-    // redux replaces this with action and reducer:
-    // onSearchChange = (e) => {
-    //     this.setState({ searchfield: e.target.value });
-    // }
-
    render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
         return robot.name
                 .toLowerCase()
                 .includes(searchField.toLowerCase());
     });
 
-    return !robots.length
-        ? <h1 className='tc ma5'>Loading...</h1>
+    return isPending ?
+        <h1 className='tc ma5'>Loading...</h1>
         : (
             <div className='tc'>
                 <h1 className='f1'>RoboLove</h1>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import {createLogger, applyMiddleware} from 'redux-logger';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import {createLogger} from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import {searchRobots} from './reducers';
+import {searchRobots, requestRobots} from './reducers';
 import 'tachyons';
 
-// use middleware between action and reducer to monitor actions:
+// use middleware between action and reducer:
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const robotReducer = combineReducers({ searchRobots, requestRobots });
+const store = 
+    createStore(robotReducer, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
     <Provider store={store}>
