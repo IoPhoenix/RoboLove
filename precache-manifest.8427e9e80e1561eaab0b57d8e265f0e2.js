5f5da2fe71e44ec8400dfc478455091ee7fdfd2b
Olga Fomin <olgafomin@oz.local>
2019-01-07 21:50:23 -0800

    Updates

---
+++ precache-manifest.8427e9e80e1561eaab0b57d8e265f0e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "86c545ccae4a76a95a9ffdd312221e69",
+    "url": "/RoboLove/static/media/MRROBOT.86c545cc.woff"
+  },
+  {
+    "revision": "ae77a68f6a7a41be1cb4",
+    "url": "/RoboLove/static/js/runtime~main.ae77a68f.js"
+  },
+  {
+    "revision": "5e037f2b29fca1a081f5",
+    "url": "/RoboLove/static/js/main.5e037f2b.chunk.js"
+  },
+  {
+    "revision": "77964839e0a1725e8309",
+    "url": "/RoboLove/static/js/1.77964839.chunk.js"
+  },
+  {
+    "revision": "5e037f2b29fca1a081f5",
+    "url": "/RoboLove/static/css/main.37c51959.chunk.css"
+  },
+  {
+    "revision": "77964839e0a1725e8309",
+    "url": "/RoboLove/static/css/1.1a7142cd.chunk.css"
+  },
+  {
+    "revision": "c27bb38074d1f76d36edc78c9bd74195",
+    "url": "/RoboLove/index.html"
+  }
+];
